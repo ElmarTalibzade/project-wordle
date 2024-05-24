1b4a41c433a5edf79dbde6b1721c0866ab2dab6e
Elmar Talibzade <elmar.talibzade@gmail.com>
2024-05-25 00:22:11 +0100

    Fix style

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import Cell from "../Cell";
-import styles from "../../styles.css";
 
 function Guess({guess}) {
     return (
-        <div className={styles.guess}>
+        <div className="guess">
             {guess.map(({letter, status}, i) => (
                 <Cell key={i} letter={letter} status={status}></Cell>
             ))}
