ff51d964847688495c8fc4b090652ede946cd220
Elmar Talibzade <elmar.talibzade@gmail.com>
2024-05-25 11:38:27 +0100

    Resetting game also samples new answer

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -7,27 +7,33 @@ import {checkGuess} from "../../game-helpers";
 import Guess from "../Guess";
 import Banner from "../Banner";
 
-// Pick a random word on every pageload.
-const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
-console.info({answer});
-
 const defaultGuesses = new Array(NUM_OF_GUESSES_ALLOWED).fill(new Array(MAX_LETTER_COUNT).fill({
     letter: '',
     status: '',
 }));
+
+function sampleAnswer() {
+    // Pick a random word on every pageload.
+    const answer = sample(WORDS);
+    // To make debugging easier, we'll log the solution in the console.
+    console.info({answer});
+
+    return answer;
+}
 
 function Game() {
     const [outcome, setOutcome] = useState('');
     const [guessCount, setGuessCount] = React.useState(0);
     const [guessInput, setGuessInput] = React.useState('');
     const [guesses, setGuesses] = React.useState([...defaultGuesses]);
+    const [answer, setAnswer] = React.useState(() => sampleAnswer());
 
     function resetGame() {
         setGuessCount(0);
         setGuessInput('');
         setGuesses([...defaultGuesses]);
         setOutcome('');
+        setAnswer(sampleAnswer());
     }
 
     function submitGuess(e) {
@@ -65,7 +71,8 @@ function Game() {
 
             <form className={styles.guessInputWrapper} onSubmit={submitGuess}>
                 <label htmlFor="guess-input">Enter guess:</label>
-                <input disabled={!!outcome} id="guess-input" type="text" value={guessInput} onChange={(e) => setGuessInput(e.target.value.toUpperCase())}/>
+                <input disabled={!!outcome} id="guess-input" type="text" value={guessInput}
+                       onChange={(e) => setGuessInput(e.target.value.toUpperCase())}/>
             </form>
             <button onClick={resetGame}>Reset</button>
 
